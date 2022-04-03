6d195ba6292b66bf7e6465369c271087cc9e158d
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-04-03 15:50:24 +0100

    Ex3.3

--- index.js
+++ index.js
@@ -31,6 +31,16 @@ app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(n => n.id === parseInt(id));
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+});
+
 app.get('/info', (req, res) => {
     const datetime = new Date(Date.now());
     res.send(`<p>Phonebook has info for ${persons.length} people.</p>
